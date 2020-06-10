fa2f81b6ef2593a75980f377f5f107bfa301bba3
Claudio Felix <claudio.felixdossantos@yahoo.com.br>
2020-06-10 19:19:54 -0300

    Conceitos de Construtores, sobrecarga, palavra this

---
+++ ConceitosBasicos/classes/Produto.cs
@@ -0,0 +1,101 @@
+﻿/*Conceitos basicos de Construtores, sobrecarga, palavra this, encapsulamento properties , 
+ Auto Properties, */
+ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+
+
+namespace ConceitosBasicos
+{
+    class Produto
+    {
+       
+        public String Nome;
+        public double Preco;
+        public int QtdEstoque;
+             
+
+        //Construtores 
+
+            // Referenciando outro Construtor em um Construtor com o uso da palavra this
+        
+        public Produto() //Construtor padrão
+        {
+            QtdEstoque = 0;
+            /*por padrão os atributos já recebem o valor zero*/
+        }
+
+        public Produto(String nome, double preco) : this() /*  this() referencia o construtor padrão*/
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public Produto(String nome, double preco, int qtdEstoque) : this( nome, preco) /*  this(nome, preco) referencia o construtor Produto(String nome, double preco) */
+        {           
+            QtdEstoque = qtdEstoque;
+        }
+
+       
+        
+
+        /*sobrecarga • É um recurso que uma classe possui de oferecer mais de uma
+        operação com o mesmo nome, porém com diferentes listas de
+       parâmetros*/
+
+        /*Palavra this : é uma referencia para o proprio objeto
+         * Uso comuns :
+         * Diferenciar atributos de variaveis locais(em java os atributos são iniciados com letra minuscula)
+         * Referenciar outro Construtor em um Construtor
+         * Passar o proprio objeto como argumento na chamada de um metodo ou construtor 
+             */
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * QtdEstoque;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            QtdEstoque += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            if (quantidade <= QtdEstoque)
+            {
+                QtdEstoque -= quantidade;
+            }
+            else
+            {
+                Console.WriteLine(" Quantidade no estoque menor que a solicitada ! \n");
+            }
+
+
+        }
+
+        //Sobreposição 
+
+        // A palavra  override indica que esta operação vem de outra classe 
+
+        public override string ToString() //Retorna uma cadeira de caracteres que representa o objeto atual 
+        {//Aqui definimos como o objeto será retornado na forma de String
+            return this.Nome +
+                ", $ " +
+                this.Preco.ToString("F2", CultureInfo.InvariantCulture) +
+                " "+this.QtdEstoque +
+                " unidade, total : $ " +
+                ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+
+        /* * ToString - converte um objeto para String  
+        * Utilizando o método ToString podemos escolher como o objeto será representado na forma de String*/
+
+    }
+}
---
+++ ConceitosBasicos/classes/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace ConceitosBasicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = null;
+            int op = 0;
+
+            do
+            {
+              op = Menu();
+                switch (op)
+                {
+                    case 1:
+
+                        CadastrarProduto(p);
+
+                        break;
+                    case 2:
+
+                      
+                        break;
+
+                    case 3:
+                       
+                        break;
+
+                    case 4:
+
+                        Console.WriteLine(" Programa encerrado ! \n");
+                        break;
+                }
+
+
+            } while (op != 4);           
+
+            Console.Read();
+
+        }
+
+
+        public static int Menu()
+        { 
+            Console.WriteLine(" Menu de Opções : \n" +
+                "1- Cadastrar Produto : \n" +
+                "2- Adicionar Produto : \n" +
+                "3- Remover Produto : \n"+
+                "4- Sair \n");
+
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static void CadastrarProduto(Produto p)
+        {
+            
+            Console.WriteLine(" Entre com os dados do Produto \n");
+
+            Console.WriteLine(" Entre com o nome do produto :");
+            String nome = Console.ReadLine();
+
+            Console.WriteLine(" Entre com o preço do produto :");
+             double preco = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            /*Console.WriteLine(" Entre com a quantidade do estoque do produto :");
+            int qtdEstoque = int.Parse(Console.ReadLine());*/
+
+            //p = new Produto(nome, preco);
+
+            /* Sintaxe alternativa para criação de objeto sem utilizar os construtores 
+             informando diretamente os valores 
+            p = new Produto { Nome = " ventilador", Preco = 30, QtdEstoque = 2 }; */
+
+            MostrarProduto(p);
+
+            AdicionarProduto(p);
+            RemoverProduto(p);
+
+        }
+
+        public static void AdicionarProduto(Produto p)
+        {
+            Console.WriteLine(" Entre com a quantidade que será adicionada ao estoque : \n");
+            p.AdicionarProdutos(int.Parse(Console.ReadLine()));
+            MostrarProduto(p);
+
+        }
+
+        public static void RemoverProduto(Produto p)
+        {
+            Console.WriteLine(" Entre com a quantidade que será removida do estoque : \n");
+            p.RemoverProdutos(int.Parse(Console.ReadLine()));
+            MostrarProduto(p);
+        }
+        public static void MostrarProduto(Produto p)
+        {
+            Console.WriteLine(" Produto :" + p);
+            Console.WriteLine(" \n");
+        }
+    }
+}
